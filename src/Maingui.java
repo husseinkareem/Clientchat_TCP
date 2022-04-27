f08a88599b8a9e88c9f5428ccd0f156e6566a0e8
husseinkareem <husseinkeldoon@yahoo.com>
2022-04-27 14:14:35 +0200

    client_tcp

--- src/Maingui.java
+++ src/Maingui.java
@@ -2,21 +2,21 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.io.*;
 import java.net.Socket;
+import java.util.Objects;
 
 public class Maingui extends JFrame implements Runnable {
     private JPanel contentPane;
-
-    private JButton DisconnectButton;
     private JButton connectButton;
     private JTextField textField1;
     private JTextArea textArea1;
-    private JButton Disconnect;
-    private JTextArea textArea2;
     private JTextField nameTextField;
     private JButton sendButton;
+    private JTextArea textArea2;
     Socket connectionSocket = new Socket("localhost",12345);
     PrintWriter writer;
     BufferedReader read;
+
+    String str;
 
     public Maingui() throws IOException {
         setContentPane(contentPane);
@@ -25,19 +25,25 @@ public class Maingui extends JFrame implements Runnable {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         writer = new PrintWriter(new OutputStreamWriter(connectionSocket.getOutputStream()));
         read = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-
+        sendButton.setEnabled(false);
 
         connectButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                if(checkIfOnline(nameTextField.getText().trim())){
+                    textArea1.append("Try again, with other name");
+                }
+                else {
+                    sendButton.setEnabled(true);
+                    try {
+                        send("Is online");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
             }
         });
-        Disconnect.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
 
-            }
-        });
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -51,7 +57,7 @@ public class Maingui extends JFrame implements Runnable {
         });
     }
     private void send(String message) throws IOException{
-        writer.println(message);
+        writer.println(nameTextField.getText() + " ¥ " + message);
         writer.flush();
     }
     public static void main(String[] args) throws IOException {
@@ -59,12 +65,25 @@ public class Maingui extends JFrame implements Runnable {
         Thread thread = new Thread(maingui);
         thread.start();
     }
+    private boolean checkIfOnline(String alias) {
+        for (String line : textArea2.getText().split("\\n")){
+            System.out.println(line);
+            if(line.trim().equalsIgnoreCase(alias.trim()))
+                return true;
+            }
+        return false;
+    }
     @Override
     public void run() {
         try{
             String message;
             while((message = read.readLine()) !=null){
-                textArea1.append(message +"\r\n");
+                String[] name = message.split("¥", 2);
+                String str = message.replace('¥', ':');
+                textArea1.append(str + "\r\n");
+                if(!checkIfOnline(name[0])) {
+                    textArea2.append(name[0] +"\n");
+                }
         }
     } catch (IOException e) {
             throw new RuntimeException(e);
